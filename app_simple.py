e3f3d5797ddaabd7801f8055717d4e80ab7d07b5
Clara <clar.holst@gmail.com>
2025-07-09 16:58:04 +0200

    deploy

---
+++ app_simple.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about_us.html')
+
+@app.route('/how_to_donate')
+def donate():
+    return render_template('how_to_donate.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Change to False for production
